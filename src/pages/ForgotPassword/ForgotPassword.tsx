49b7b18f54442f028a104461239de8f65b15ec20
diego41071 <alexander.diego.2007@hotmail.com>
2024-11-04 15:34:21 -0500

    cambios en contraseña

--- src/pages/ForgotPassword/ForgotPassword.tsx
+++ src/pages/ForgotPassword/ForgotPassword.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { IonButton, IonInput, IonPage } from "@ionic/react";
+import { IonButton, IonInput, IonItem, IonLabel, IonPage } from "@ionic/react";
 import axios from "axios";
 import { handleForgotPassword } from "../../services/authService";
 
@@ -8,17 +8,24 @@ const ForgotPassword: React.FC = () => {
 
   return (
     <IonPage>
-      <IonInput
-        placeholder="Ingrese su correo"
-        value={email}
-        onIonChange={(e) => setEmail(e.detail.value!)}
-      />
-      <IonButton
-        onClick={(e) => handleForgotPassword(email)}
-        routerLink="/resetpassword"
-      >
-        Recuperar Contraseña
-      </IonButton>
+      <IonItem className="custom-item">
+        <IonLabel className="custom-label-login">Correo electrónico</IonLabel>
+        <IonInput
+          placeholder="Ingrese su correo"
+          value={email}
+          onIonChange={(e) => setEmail(e.detail.value!)}
+          className="custom-input"
+        />
+      </IonItem>
+      <div className="container-button">
+        <IonButton
+          onClick={(e) => handleForgotPassword(email)}
+          routerLink="/resetpassword"
+          className="custom-button"
+        >
+          Recuperar Contraseña
+        </IonButton>
+      </div>
     </IonPage>
   );
 };
