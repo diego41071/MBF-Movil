2e9c30aeb7d13f3271c115a780be0a3bc4a60f5a
diego41071 <alexander.diego.2007@hotmail.com>
2024-11-26 22:30:43 -0500

    cambos en roles

--- src/components/ExplorerContainer/ExploreContainer.tsx
+++ src/components/ExplorerContainer/ExploreContainer.tsx
@@ -17,53 +17,117 @@ export default function ExploreContainer(props: { role: string }) {
   };
   return (
     <IonContent className="ion-padding">
+      <IonTitle className="text-center">{props.role}</IonTitle>
       {props.role === "Cliente" ? (
-        <>
-          <IonTitle className="text-center">{props.role}</IonTitle>
-          <div className="profile-grid">
-            {[
-              { name: "Cronograma", icon: calendarOutline, route: "/schedule" },
-              {
-                name: "Fichas técnicas",
-                icon: readerOutline,
-                route: "/technicaldatasheet",
-              },
-              {
-                name: "Solicitar servicio técnico",
-                icon: buildOutline,
-                route: "/technicalservice",
-              },
-              { name: "Informe", icon: clipboardOutline, route: "/report" },
-              { name: "Escaneo de QR", icon: qrCodeOutline, route: "/qrscan" },
-              {
-                name: "Báscula por bluetooth",
-                icon: bluetoothOutline,
-                route: "/bluetooth",
-              },
-            ].map((item) => {
-              return (
-                <div
-                  className="profile-container"
-                  onClick={(e) => redirectPages(item.route)}
-                >
-                  <div className="image-container">
-                    <div className="custom-image">
-                      <IonIcon
-                        className="custom-icon-explore"
-                        icon={item.icon}
-                      />
-                    </div>
+        <div className="profile-grid">
+          {[
+            { name: "Cronograma", icon: calendarOutline, route: "/schedule" },
+            {
+              name: "Fichas técnicas",
+              icon: readerOutline,
+              route: "/technicaldatasheet",
+            },
+            {
+              name: "Solicitar servicio técnico",
+              icon: buildOutline,
+              route: "/technicalservice",
+            },
+            {
+              name: "Servicios técnicos solicitados",
+              icon: clipboardOutline,
+              route: "/report",
+            },
+            { name: "Escaneo de QR", icon: qrCodeOutline, route: "/qrscan" },
+            {
+              name: "Báscula por bluetooth",
+              icon: bluetoothOutline,
+              route: "/bluetooth",
+            },
+          ].map((item) => {
+            return (
+              <div
+                className="profile-container"
+                onClick={(e) => redirectPages(item.route)}
+              >
+                <div className="image-container">
+                  <div className="custom-image">
+                    <IonIcon className="custom-icon-explore" icon={item.icon} />
                   </div>
-                  <p>{item.name}</p>
                 </div>
-              );
-            })}
-          </div>
-        </>
+                <p>{item.name}</p>
+              </div>
+            );
+          })}
+        </div>
       ) : props.role === "Administrador" ? (
-        "Rol administrador"
+        <div className="profile-grid">
+          {[
+            { name: "Cronograma", icon: calendarOutline, route: "/schedule" },
+            {
+              name: "Fichas técnicas",
+              icon: readerOutline,
+              route: "/technicaldatasheet",
+            },
+            {
+              name: "Solicitar servicio técnico",
+              icon: buildOutline,
+              route: "/technicalservice",
+            },
+            { name: "Informe", icon: clipboardOutline, route: "/report" },
+            { name: "Escaneo de QR", icon: qrCodeOutline, route: "/qrscan" },
+            {
+              name: "Báscula por bluetooth",
+              icon: bluetoothOutline,
+              route: "/bluetooth",
+            },
+          ].map((item) => {
+            return (
+              <div
+                className="profile-container"
+                onClick={(e) => redirectPages(item.route)}
+              >
+                <div className="image-container">
+                  <div className="custom-image">
+                    <IonIcon className="custom-icon-explore" icon={item.icon} />
+                  </div>
+                </div>
+                <p>{item.name}</p>
+              </div>
+            );
+          })}
+        </div>
       ) : props.role === "Tecnico" ? (
-        "Rol técnico"
+        <div className="profile-grid">
+          {[
+            { name: "Cronograma", icon: calendarOutline, route: "/schedule" },
+            {
+              name: "Fichas técnicas",
+              icon: readerOutline,
+              route: "/technicaldatasheet",
+            },
+            { name: "Informe", icon: clipboardOutline, route: "/report" },
+            { name: "Escaneo de QR", icon: qrCodeOutline, route: "/qrscan" },
+            {
+              name: "Báscula por bluetooth",
+              icon: bluetoothOutline,
+              route: "/bluetooth",
+            },
+          ].map((item) => {
+            return (
+              <div
+                className="profile-container"
+                onClick={(e) => redirectPages(item.route)}
+              >
+                <div className="image-container">
+                  <div className="custom-image">
+                    <IonIcon className="custom-icon-explore" icon={item.icon} />
+                  </div>
+                </div>
+                <p>{item.name}</p>
+              </div>
+            );
+          })}
+        </div>
       ) : (
         "Este usuario no tiene un rol asignado"
       )}
