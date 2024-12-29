bdfb686973fc77787f20f3365976959e4fbb61b9
diego41071 <alexander.diego.2007@hotmail.com>
2024-12-28 20:15:18 -0500

    cuando se da en boton de atras en inicio que no se devuelva al login

--- src/pages/Page/Page.tsx
+++ src/pages/Page/Page.tsx
@@ -8,11 +8,29 @@ import {
   IonToolbar,
 } from "@ionic/react";
 import { useParams } from "react-router";
+import { useHistory } from "react-router-dom";
 import ExploreContainer from "../../components/ExplorerContainer/ExploreContainer";
 import "./Page.css";
+import { useEffect } from "react";
+import { App } from "@capacitor/app";
 
 export default function Page(props: { role: any }) {
   const { name } = useParams<{ name: string }>();
+  const history = useHistory();
+
+  useEffect(() => {
+    const backButtonListener = App.addListener("backButton", () => {
+      // Previene volver al login si estás en la página de inicio
+      if (history.location.pathname === "/page") {
+        // Puedes mostrar un mensaje de confirmación si deseas
+        App.exitApp(); // Cierra la app
+      }
+    });
+
+    return () => {
+      backButtonListener.remove(); // Limpia el listener cuando el componente se desmonta
+    };
+  }, [history]);
 
   return (
     <IonPage>
